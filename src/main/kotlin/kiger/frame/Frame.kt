4db30ce61a29b66afbb8b46b4c698b0c241daa6f
Juha Komulainen <juha.komulainen@evident.fi>
2016-04-26 10:50:20 +0300

    More stuff to frames

--- src/main/kotlin/kiger/frame/Frame.kt
+++ src/main/kotlin/kiger/frame/Frame.kt
@@ -12,6 +12,7 @@ abstract class Frame(val name: Label, private val formalEscapes: List<Boolean>) 
         get() = TODO()
 
     fun allocLocal(escape: Boolean): Access = TODO()
+
     fun procEntryExit1(body: TreeStm): TreeStm = TODO()
 }
 
@@ -19,19 +20,35 @@ class JouletteFrame(name: Label, formalEscapes: List<Boolean>) : Frame(name, for
 
     companion object : FrameType {
         override val FP = Temp("fp")
+        override val SP = Temp("sp")
         override val RV = Temp("rv")
+        override val RA = Temp("ra")
         override val wordSize = 4
         override fun newFrame(name: Label, formalEscapes: List<Boolean>) = JouletteFrame(name, formalEscapes)
         override fun exp(access: FrameAccess, exp: TreeExp) = TODO()
         override fun externalCall(name: String, args: List<TreeExp>) = TODO()
+        override val argumentRegisters: List<Temp>
+            get() = TODO()
+        override val callerSaves: List<Temp>
+            get() = TODO()
+        override val calleeSaves: List<Temp>
+            get() = TODO()
     }
 }
 
 interface FrameType {
-    val FP: Temp
-    val RV: Temp
-    val wordSize: Int
     fun newFrame(name: Label, formalEscapes: List<Boolean>): Frame
+    val FP: Temp
+    val SP: Temp
+    val RV: Temp
+    val RA: Temp
+    val wordSize: Int
+
+    val argumentRegisters: List<Temp>
+    val callerSaves: List<Temp>
+    val calleeSaves: List<Temp>
+
     fun exp(access: FrameAccess, exp: TreeExp): TreeExp
+
     fun externalCall(name: String, args: List<TreeExp>): TreeExp
 }
