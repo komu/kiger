6201164a08f8fb97369b483118b4c0e1e1b80549
Juha Komulainen <juha.komulainen@evident.fi>
2016-05-03 22:01:45 +0300

    Restructure evaluator

--- vm/src/main/kotlin/kiger/vm/Evaluator.kt
+++ vm/src/main/kotlin/kiger/vm/Evaluator.kt
@@ -11,29 +11,33 @@ class Evaluator(allInstructions: List<Inst>) {
     val PC_EXIT = -1
     var pc = PC_EXIT
 
-    val insts = mutableListOf<Inst>()
+    val instructions = mutableListOf<Instruction>()
+    val originalInstructions = mutableListOf<Inst>()
     val labelMap = mutableMapOf<String, Int>()
 
-    val RA = "\$ra"
-    val V0 = "\$v0"
-    val A1 = "\$a0"
-    val FP = "\$fp"
-    val SP = "\$sp"
     var trace = false
 
-    private val mem = Array(1024 * 1024) { 0 }
+    val mem = Array(1024 * 1024) { 0 }
 
     init {
         for (inst in allInstructions) {
-            if (inst is Inst.Label)
-                labelMap[inst.name] = insts.size
-            else
-                insts += inst
+            if (inst is Inst.Label) {
+                labelMap[inst.name] = instructions.size
+            } else {
+                originalInstructions += inst
+                instructions += when (inst) {
+                    is Op0 -> inst.analyze()
+                    is Op1 -> inst.analyze(regs)
+                    is Op2 -> inst.analyze(regs)
+                    is Op3 -> inst.analyze(regs)
+                    else   -> error("unknown instruction: $inst")
+                }
+            }
         }
 
-        regs[RA] = PC_EXIT
-        regs[FP] = mem.size - 1000
-        regs[SP] = mem.size - 1000
+        regs.ra.value = PC_EXIT
+        regs.fp.value = mem.size - 1000
+        regs.sp.value = mem.size - 1000
     }
 
     fun run() {
@@ -43,89 +47,100 @@ class Evaluator(allInstructions: List<Inst>) {
             step()
         }
 
-        println("v0: ${regs[V0]}")
+        println("v0: ${regs.v0.value}")
     }
 
     fun step() {
         if (trace)
-            println("$pc: ${insts[pc].toString().padEnd(30)} $regs")
+            println("$pc: ${instructions[pc].toString().padEnd(30)} $regs")
 
-        val op = insts[pc++] as? Inst.Op
-
-        when (op) {
-            is Op0 -> op.eval()
-            is Op1 -> op.eval()
-            is Op2 -> op.eval()
-            is Op3 -> op.eval()
+        val oldPc = pc
+        try {
+            pc++
+            instructions[oldPc].eval(this)
+        } catch (e: Exception) {
+            System.err?.println("Exception when evaluating ${instructions[oldPc]}")
+            e.printStackTrace()
+            pc = PC_EXIT
         }
     }
 
-    private fun Op0.eval() {
-        when (name) {
-            "syscall"   -> syscall()
-            else        -> error("Unsupported op: $this")
-        }
-    }
-
-    private fun syscall() {
-        val call = regs[V0]
+    fun syscall() {
+        val call = regs.v0.value
         when (call) {
-            1   -> System.out.print(regs[A1])
-            4   -> System.out.print((insts[regs[A1]] as Inst.Data).text)
+            1   -> System.out.print(regs.a1.value)
+            4   -> System.out.print((originalInstructions[regs.a1.value] as Inst.Data).text)
             10  -> pc = PC_EXIT
             else -> error("unknown syscall $call")
         }
     }
 
-    private fun Op1.eval() {
-        when (name) {
-            "j"     -> pc = o1.immediate
-            "jr"    -> pc = regs[o1.reg]
-            "jal"   -> {
-                regs[RA] = pc
-                pc = o1.immediate
-            }
-            "jalr"   -> {
-                regs[RA] = pc
-                pc = regs[o1.reg]
-            }
-            else -> error("Unsupported op: $this")
-        }
-    }
-
-    private fun Op2.eval() {
-        when (name) {
-            "move"  -> regs[o1.reg] = regs[o2.reg]
-            "lw"    -> regs[o1.reg] = mem[regs[o2.baseReg] + o2.offset]
-            "sw"    -> mem[regs[o2.baseReg] + o2.offset] = regs[o1.reg]
-            "li"    -> regs[o1.reg] = o2.immediate
-            "la"    -> regs[o1.reg] = o2.immediate
-            else    -> error("Unsupported op: $this")
-        }
-    }
-
-    private fun Op3.eval() {
-        when (name) {
-            "add"   -> regs[o1.reg] = regs[o2.reg] + regs[o3.reg]
-            "addiu" -> regs[o1.reg] = regs[o2.reg] + o3.immediate
-            "mul"   -> regs[o1.reg] = regs[o2.reg] * regs[o3.reg]
-            else -> error("Unsupported op: $this")
-        }
-    }
-
-    private val Operand.immediate: Int
+    val Operand.immediate: Int
         get() = immediate(labelMap)
 }
 
-class Registers {
-
-    private val regs = mutableMapOf<String,Int>()
-
-    operator fun get(name: String): Int = regs[name] ?: 0
-
-    operator fun set(name: String, value: Int) {
-        regs[name] = value
+private fun Op0.analyze(): Instruction =
+    when (name) {
+        "syscall"   -> Instruction(this) { syscall() }
+        else        -> error("Unsupported op: $this")
     }
 
-    override fun toString() = regs.map { Pair(it.key, it.value) }.filter { it.second != 0 }.toString()
+private fun Op1.analyze(regs: Registers): Instruction =
+    when (name) {
+        "j"     -> Instruction(this) { pc = o1.immediate }
+        "jr"    -> { val r = regs[o1.reg]; Instruction(this) { pc = r.value } }
+        "jal"   -> Instruction(this) {
+            regs.ra.value = pc
+            pc = o1.immediate
+        }
+        "jalr"   -> {
+            val r = regs[o1.reg]
+            Instruction(this) {
+                regs.ra.value = pc
+                pc = r.value
+            }
+        }
+        else -> error("Unsupported op: $this")
+    }
+
+private fun Op2.analyze(regs: Registers): Instruction =
+    when (name) {
+        "move"  -> { val d = regs[o1.reg]; val s = regs[o2.reg]; Instruction(this) { d.value = s.value } }
+        "lw"    -> { val d = regs[o1.reg]; val b = regs[o2.baseReg]; val o = o2.offset; Instruction(this) { d.value = mem[b.value + o] } }
+        "sw"    -> { val b = regs[o2.baseReg]; val o = o2.offset; val s = regs[o1.reg]; Instruction(this) { mem[b.value + o] = s.value } }
+        "li"    -> { val d = regs[o1.reg]; Instruction(this) { d.value = o2.immediate } }
+        "la"    -> { val d = regs[o1.reg]; Instruction(this) { d.value = o2.immediate } }
+        else    -> error("Unsupported op: $this")
+    }
+
+private fun Op3.analyze(regs: Registers): Instruction =
+    when (name) {
+        "add"   -> { val d = regs[o1.reg]; val l = regs[o2.reg]; val r = regs[o3.reg]; Instruction(this) { d.value = l.value + r.value } }
+        "addiu" -> { val d = regs[o1.reg]; val l = regs[o2.reg]; Instruction(this) { d.value = l.value + o3.immediate } }
+        "mul"   -> { val d = regs[o1.reg]; val l = regs[o2.reg]; val r = regs[o3.reg]; Instruction(this) { d.value = l.value * r.value } }
+        else -> error("Unsupported op: $this")
+    }
+
+class Instruction(val inst: Inst, val eval: Evaluator.() -> Unit) {
+    override fun toString() = inst.toString()
+}
+
+class Register(val name: String, var value: Int = 0)
+
+class Registers {
+
+    private val regs = listOf("zero", "at", "v0", "v1", "a0", "a1", "a2", "a3",
+            "t0", "t1", "t2", "t3", "t4", "t5", "t6", "t7",
+            "s0", "s1", "s2", "s3", "s4", "s5", "s6", "s7",
+            "t8", "t9", "k0", "k1", "gp", "sp", "fp", "ra")
+            .map { Register("\$" + it) }.toTypedArray()
+
+    val ra = this["\$ra"]
+    val v0 = this["\$v0"]
+    val a1 = this["\$a0"]
+    val fp = this["\$fp"]
+    val sp = this["\$sp"]
+
+    operator fun get(name: String): Register = regs.find { it.name == name } ?: error("no such register: '$name'")
+    override fun toString() = regs.filter { it.value != 0 }.toString()
 }
