bc8d59ba09983f2c525c12be12db28db85de45e6
Juha Komulainen <juha.komulainen@evident.fi>
2016-05-12 21:54:11 +0300

    Remove moved classes

--- compiler/src/main/kotlin/kiger/tree/BinaryOp.kt
+++
@@ -1,7 +0,0 @@
-package kiger.tree
-
-enum class BinaryOp(val sym: String) {
-    PLUS("+"), MINUS("-"), MUL("*"), DIV("/"), AND("*"), OR("||"), LSHIFT("<<"), RSHIFT(">>"), XOR("^");
-
-    override fun toString() = sym
-}
